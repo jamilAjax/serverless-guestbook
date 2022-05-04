f978e0b8e54afd82abdc1ad8f726fb7334306671
jamilAjax <58570489+jamilAjax@users.noreply.github.com>
2022-05-04 10:47:48 -0500

    Update guestbook.js
    update api url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://0e0fa133.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
